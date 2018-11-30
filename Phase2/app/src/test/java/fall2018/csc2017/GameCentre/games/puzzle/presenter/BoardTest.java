e3a0c9a0fbb1987d89334a366666e234c81fb68a
Jose Casas <jose.casas@mail.utoronto.ca>
2018-11-30 00:10:56 -0500

    Updated BoardManagerTest, TileTest and created BoardTest and TileStateTest.

--- Phase2/app/src/test/java/fall2018/csc2017/GameCentre/games/puzzle/presenter/BoardTest.java
+++ Phase2/app/src/test/java/fall2018/csc2017/GameCentre/games/puzzle/presenter/BoardTest.java
@@ -1,7 +1,93 @@
 package fall2018.csc2017.GameCentre.games.puzzle.presenter;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class BoardTest {
 
+    /**
+     * Make a set of tiles that are not in order.
+     *
+     * @return a set of tiles that are in order
+     */
+    private List<Tile> setUpUnorderedTiles() {
+        List<Tile> tiles = new ArrayList<>();
+        final int numTiles = 4 * 4;
+        int[] unorderedTiles = {13, 15, 1, 5, 8, 2, 12, 10, 14, 3, 4, 7, 11, 0, 6, 9};
+        for (int tileNum = 0; tileNum != numTiles; tileNum++) {
+            tiles.add(new Tile(unorderedTiles[tileNum] + 1, numTiles));
+        }
+
+        return tiles;
+    }
+
+    /*
+    BEGIN TESTS
+     */
+    /**
+     *
+     */
+    @Test
+    public void TestConstructorWithListOfTiles() {
+        List<Tile> unorderedTiles = setUpUnorderedTiles();
+        Board testBoard = new Board(unorderedTiles, 4, 4);
+
+        assertEquals(4, testBoard.getNumRows());
+        assertEquals(4, testBoard.getNumCols());
+        assertEquals(unorderedTiles.get(0), testBoard.getTile(0, 0));
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void TestConstructorWithBoardString() {
+
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void testGetTile() {
+
+    }
+
+//    @Test
+//    public void toString() {
+//    }
+
+    /**
+     *
+     */
+    @Test
+    public void testExchangeTiles() {
+
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void testIterator() {
+        Board testBoard = new Board(setUpUnorderedTiles(), 4, 4);
+        Iterator<Tile> iter = testBoard.iterator();
+        assertTrue(iter.hasNext());
+        assertEquals(0, iter.next().getBackground());
+        assertEquals(16, iter.next().getId());
+        int[] unorderedTiles = {13, 15, 1, 5, 8, 2, 12, 10, 14, 3, 4, 7, 11, 0, 6, 9};
+        int i = 0;
+        for (Tile T : testBoard) {
+            assertEquals(T.getId(), unorderedTiles[i] + 1);
+            assertEquals(T.getBackground(), i);
+            i++;
+            iter.next();
+        }
+        assertTrue(!iter.hasNext());
+    }
 }
---
+++ Phase2/app/src/test/java/fall2018/csc2017/GameCentre/games/puzzle/presenter/TileStateTest.java
@@ -0,0 +1,52 @@
+package fall2018.csc2017.GameCentre.games.puzzle.presenter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TileStateTest {
+
+    @Test
+    public void getHighScore() {
+    }
+
+    @Test
+    public void setHighScore() {
+    }
+
+    @Test
+    public void getLatestMoveStr() {
+    }
+
+    @Test
+    public void getDimensions() {
+    }
+
+    @Test
+    public void setDimensions() {
+    }
+
+    @Test
+    public void getMoves() {
+    }
+
+    @Test
+    public void setMoves() {
+    }
+
+    @Test
+    public void getUndos() {
+    }
+
+    @Test
+    public void setUndos() {
+    }
+
+    @Test
+    public void getTime() {
+    }
+
+    @Test
+    public void setTime() {
+    }
+}
