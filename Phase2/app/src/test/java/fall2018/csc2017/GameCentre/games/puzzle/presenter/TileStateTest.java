fd8e31f031f9ed358b8b717a38d27c543ec664bb
Jose Casas <jose.casas@mail.utoronto.ca>
2018-11-30 01:03:02 -0500

    Updated TileStateTest.

--- Phase2/app/src/test/java/fall2018/csc2017/GameCentre/games/puzzle/presenter/TileStateTest.java
+++ Phase2/app/src/test/java/fall2018/csc2017/GameCentre/games/puzzle/presenter/TileStateTest.java
@@ -2,51 +2,67 @@ package fall2018.csc2017.GameCentre.games.puzzle.presenter;
 
 import org.junit.Test;
 
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 public class TileStateTest {
+    Map<String, String> moves;
+    private TileState tileState = new TileState(moves, 1, "time", "dimensions", "highscore");
 
     @Test
-    public void getHighScore() {
+    public void testGetHighScore() {
+        assertEquals("highscore", tileState.getHighScore());
     }
 
     @Test
-    public void setHighScore() {
+    public void testSetHighScore() {
+        tileState.setHighScore("new_highscore");
+        assertEquals("new_highscore", tileState.getHighScore());
+    }
+
+//    @Test
+//    public void testGetLatestMoveStr() {
+//    }
+
+    @Test
+    public void testGetDimensions() {
+        assertEquals("dimensions", tileState.getDimensions());
     }
 
     @Test
-    public void getLatestMoveStr() {
+    public void testSetDimensions() {
+        tileState.setDimensions("new_dimensions");
+        assertEquals("new_dimensions", tileState.getDimensions());
+    }
+
+//    @Test
+//    public void testGetMoves() {
+//    }
+
+//    @Test
+//    public void testSetMoves() {
+//    }
+
+    @Test
+    public void testGetUndos() {
+        assertEquals(1, tileState.getUndos());
     }
 
     @Test
-    public void getDimensions() {
+    public void testSetUndos() {
+        tileState.setUndos(2);
+        assertEquals(2, tileState.getUndos());
     }
 
     @Test
-    public void setDimensions() {
+    public void testGetTime() {
+        assertEquals("time", tileState.getTime());
     }
 
     @Test
-    public void getMoves() {
-    }
-
-    @Test
-    public void setMoves() {
-    }
-
-    @Test
-    public void getUndos() {
-    }
-
-    @Test
-    public void setUndos() {
-    }
-
-    @Test
-    public void getTime() {
-    }
-
-    @Test
-    public void setTime() {
+    public void testSetTime() {
+        tileState.setTime("new_time");
+        assertEquals("new_time", tileState.getTime());
     }
 }
